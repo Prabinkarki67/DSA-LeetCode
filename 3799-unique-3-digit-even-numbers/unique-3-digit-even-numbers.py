f3173e2587a73a2ceed1d479afa46d327f3482eb
Prabin Karki <161217370+Prabinkarki67@users.noreply.github.com>
2026-01-08 11:30:49 +0545

    Time: 11 ms (88.30%) | Memory: 19.4 MB (5.13%) - LeetSync

--- 3799-unique-3-digit-even-numbers/unique-3-digit-even-numbers.py
+++ 3799-unique-3-digit-even-numbers/unique-3-digit-even-numbers.py
@@ -1,6 +1,6 @@
 class Solution:
     def totalNumbers(self, digits: List[int]) -> int:
-        n=len(digits)
+        '''n=len(digits)
         res = []
         for i in range(n):
             for j in range(n):
@@ -14,4 +14,11 @@ class Solution:
                             num = digits[i]*100+digits[j]*10+digits[k]
                             if num not in res and num%2 == 0 and num>=100:
                                 res.append(num)
+        return len(res)
+        '''
+        from itertools import permutations
+        res = set()
+        for a,b,c in permutations(digits, 3):
+            if a!=0 and c%2 ==0:
+                res.add((a,b,c))
         return len(res)
