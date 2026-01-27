0a88df21127a22a84211b357b336e41a6646ec5b
Prabin Karki <161217370+Prabinkarki67@users.noreply.github.com>
2026-01-27 16:08:35 +0545

    Time: 0 ms (100.00%) | Memory: 19.5 MB (8.51%) - LeetSync

---
+++ 908-middle-of-the-linked-list/middle-of-the-linked-list.py
@@ -0,0 +1,29 @@
+# Definition for singly-linked list.
+# class ListNode:
+#     def __init__(self, val=0, next=None):
+#         self.val = val
+#         self.next = next
+class Solution:
+    def middleNode(self, head: Optional[ListNode]) -> Optional[ListNode]:
+        def trav(head):
+            curr=head
+            counter = 0
+            while curr:
+                counter+=1
+                curr=curr.next
+            return counter
+        count = trav(head)
+        def res(head, counter):
+            curr=head
+            indx=1
+            while curr:
+                if counter ==indx:
+                    return curr
+                indx+=1
+                curr=curr.next
+        if count%2!=0:  #odd
+            n= (count+1)//2
+        else:
+            n=(count+2)//2
+        
+        return res(head, n)
