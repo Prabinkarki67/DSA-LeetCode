53724740285748f8c02e76adae5f89193b067d9f
Prabin Karki <161217370+Prabinkarki67@users.noreply.github.com>
2026-06-03 09:13:45 +0545

    Time: 11 ms (84.21%) | Memory: 22 MB (67.01%) - LeetSync

--- 49-group-anagrams/group-anagrams.py
+++ 49-group-anagrams/group-anagrams.py
@@ -1,11 +1,19 @@
 class Solution:
     def groupAnagrams(self, strs: List[str]) -> List[List[str]]:
             hash_mp = {}
+            # make hashtable or dict like:
+            # {
+            #    "aet": ["eat", "tea", "ate"],
+            #    "ant": ["tan", "nat"],
+            #    "abt": ["bat"]
+            #}
             for ch in strs:
                 key = ''.join(sorted(ch))
                 if key not in hash_mp:
                     hash_mp[key] = []
                 hash_mp[key].append(ch)
+
+            # make dist value in list 
             res = []
             for i in hash_mp:
                 res.append(hash_mp[i])
